8c11b2a71104b31777ad755d9cbc735010b9305d
hassanaya <hassanaya1@gmail.com>
2023-08-12 13:34:54 -0700

    u

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Description: u
+ * Return: 0
+*/
+
+int main(void)
+{
+	int d = 0;
+
+	while (d <= 9)
+	{
+		putchar(d + '0');
+		putchar(',');
+		putchar(' ');
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
