63258e3a02d560287753179031c2471f9e660434
hassanaya <hassanaya1@gmail.com>
2023-08-12 14:19:53 -0700

    u

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Description: u
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i = 0, j = 0, k = 0;
+
+	for (i = 0 ; i < 8 ; i++)
+	{
+		for (j  = i + 1 ; j < 9 ; j++)
+		{
+			for (k = j + 1 ; k <= 9 ; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (!(i == 8 && j == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
