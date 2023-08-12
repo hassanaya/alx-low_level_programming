e4705ce34bf6a644842eccc3614add36b4b7685f
hassanaya <hassanaya1@gmail.com>
2023-08-12 12:31:36 -0700

    u

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Description: u
+ * Return: 0
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
