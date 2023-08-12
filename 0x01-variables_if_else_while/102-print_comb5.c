a4ef6df55666c5c042eb9a9920db660592c2f50a
hassanaya <hassanaya1@gmail.com>
2023-08-12 14:40:03 -0700

    u

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,7 +21,7 @@ int main(void)
 				putchar(' ');
 				putchar((j / 10) + '0');
 				putchar((j % 10) + '0');
-				if (!(i == 89 && j == o99))
+				if (!(i == 89 && j == 99))
 				{
 					putchar(',');
 					putchar(' ');
