a0fc45bfab3f0be8fd433ea22b10c268e974a6c9
hassanaya <hassanaya1@gmail.com>
2023-08-12 14:35:29 -0700

    u

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Description: u
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i = 0, j = 0;
+
+	for (i = 0 ; i < 99 ; i++)
+	{
+		for (j  = i + 1 ; j <= 99 ; j++)
+		{
+			if (i != j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (!(i == 89 && j == 99))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
