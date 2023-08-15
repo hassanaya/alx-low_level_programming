87b5627a31727f7a6bd5c2ac5ac984ecb3ba212b
hassanaya <hassanaya1@gmail.com>
2023-08-15 07:23:21 -0700

    u

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_to_98 - adf
+ * @x: int
+ * Return: 0
+*/
+
+void print_to_98(int n)
+{
+	int num;
+
+	printf("%i", num);
+	if (num < 98)
+	{
+		for (num = n  ; num < 98 ; num++)
+		{
+			printf("%i, ", num);
+		}
+	}
+	else if (num > 98)
+	{
+		for (num = n ; num > 98 ; num--)
+		{
+			printf("%i, ", num);
+		}
+	}
+	printf("98/n");
+}
