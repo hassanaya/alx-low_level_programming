26a4188601af93c03aef48f05abfdf09e6ef6fce
hassanaya <hassanaya1@gmail.com>
2023-08-18 09:31:33 -0700

    u

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,10 @@ void more_numbers(void)
 	{
 		for (i = 0 ; i < 15 ; i++)
 		{
-			_putchar(i + '0');
+			if (i > 9)
+				_putchar('1');
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
