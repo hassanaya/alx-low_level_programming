d2a8a81f232389fac6509678454498ba5cafb0d4
hassanaya <hassanaya1@gmail.com>
2023-08-25 18:32:37 -0700

    U

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - returns bool
+ * @s1: pointer
+ * @s2: pointer
+ * Return: pointer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, ret = 0;
+
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+	{
+		i++;
+	}
+	if (s1[i] == s2[i])
+		return (0);
+	ret = s1[i] - s2[i] - '0';
+	return (ret);
+}
