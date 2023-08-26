dc32cb8932bca7eefafc0f563c58cb19b1d6bc18
hassanaya <hassanaya1@gmail.com>
2023-08-25 18:40:22 -0700

    U

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,6 +17,6 @@ int _strcmp(char *s1, char *s2)
 	}
 	if (s1[i] == s2[i])
 		return (0);
-	ret =(int) s1[i] - '\0' -(int) s2[i] - '0';
+	ret =((int) s1[i] - '\0)' -((int) s2[i] - '0');
 	return (ret);
-}
+	}
