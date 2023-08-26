06f42148a802c8ba0ca08ab05b273f0e2a21397a
hassanaya <hassanaya1@gmail.com>
2023-08-25 18:02:56 -0700

    u

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - returns bool
+ * @dest: pointer
+ * @src: pointer
+ * @n: int
+ * Return: pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && n != 0)
+	{
+		dest[i] = src[i];
+		i++;
+		n--;
+	}
+	dest[count] = '\0';
+	return (dest);
+}
