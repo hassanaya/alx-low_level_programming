c21572e483fb8d6cc44754c3f9afba126ec9ac0f
hassanaya <hassanaya1@gmail.com>
2023-08-29 03:42:51 -0700

    u

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,14 +12,12 @@ char *_strstr(char *haystack, char *needle)
 	int i, j;
 	char start = needle[0];
 
-	if (start != '\0' || start == '\0')
+	if (start != '\0')
 	{
-		for  (j = 0 ; haystack[j] >= '\0' ; j++)
+		for  (j = 0 ; haystack[j] != '\0' ; j++)
 		{
 			if (start  == haystack[j])
 			{
-				if (haystack[j] == '\0')
-					return (&haystack[j]);
 				for (i = 1 ; ; i++)
 				{
 					if (needle[i] == '\0')
