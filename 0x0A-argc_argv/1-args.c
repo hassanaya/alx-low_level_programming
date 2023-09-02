21823d7283cfeff7d8840a3233cf9d3c3abe2386
hassanaya <hassanaya1@gmail.com>
2023-09-01 18:18:29 -0700

    u

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @argc: int
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%d\n", argc);
+	return (0);
+}
