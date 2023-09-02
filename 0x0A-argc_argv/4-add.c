df046a55b12190936c7fe4b6750841a1efc176c4
hassanaya <hassanaya1@gmail.com>
2023-09-01 19:38:37 -0700

    u

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,20 +2,27 @@
 #include <stdlib.h>
 
 /**
+ * isStringInt - entry
+ * @str: array
+ * Return: 0
+ */
+
+int isStringInt(const char *str)
+{
+	char *endptr;
+
+	strtol(str, &endptr, 10);
+	if (*str != '\0' && *endptr == '\0')
+		return (1);
+	return (0);
+}
+
+/**
  * main - entry
  * @argc: int
  * @argv: array
  * Return: 0
  */
-
-int isStringInt(const char* str) {
-        char* endptr;
-
-        strtol(str, &endptr, 10);
-        if (*str != '\0' && *endptr == '\0')
-		return (1);
-	return (0);
-}
 
 int main(int argc, char const *argv[])
 {
