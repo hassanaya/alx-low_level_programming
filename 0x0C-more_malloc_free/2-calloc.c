1ec644881a9981816351e87311676108eb9bab87
hassanaya <hassanaya1@gmail.com>
2023-09-08 16:35:19 -0700

    u

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_memset - returns void
+ *@s: pointer
+ *@b: int
+ *@n: ubt
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+	while (n--)
+		*s++ = b;
+	return (ptr);
+}
+/**
+ *_calloc - returns void
+ *@nmemb: pointer
+ *@size: int
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+	int totalSize = size * nmemb;
+
+	if (size == 0 || nmemb == 0)
+		return  (NULL);
+	m = malloc(totalSize);
+
+	if (m == 0)
+		return (NULL);
+	_memset(m, 0, size * nmemb);  
+	return (m);
+}
