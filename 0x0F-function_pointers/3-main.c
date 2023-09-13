e6bbed8eb0975b963eab8e1f147344432f357082
hassanaya <hassanaya1@gmail.com>
2023-09-13 14:56:51 -0700

    u

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+# include "3-calc.h"
+
+/**
+ * main - check
+ * @argc: number
+ * @argv: vector
+ * Return: always 0
+ */
+
+int main(int argc, char **argv)
+{
+	int (*op_func)(int, int), a, b;
+
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+	if (!0p_func)
+		printf("Error\n"), exit(99);
+
+	if ((!b && (argv[2][0] == '/' || argv[2][0]  == '%'))
+		printf("Error\n"), exit(100);
+
+	printf("%d\n", op_fun(a, b));
+	return (0);
+}
